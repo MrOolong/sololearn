ed7a63af9952d31399aeb2918d19bdc1a14ab283
Dennis J Daubney Jr <dennis.daubney@gmail.com>
2017-12-27 14:05:50 -0500

    basic calculator - initial commit

---
+++ csharp/BasicCalculator/BasicCalculator/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            do
+            {
+                Console.Write("x = ");
+                int x = Convert.ToInt32(Console.ReadLine());
+
+                Console.Write("y = ");
+                int y = Convert.ToInt32(Console.ReadLine());
+
+                int sum = x + y;
+                Console.WriteLine("Result: {0}", sum);
+            }
+            while (true);
+        }
+    }
+}
