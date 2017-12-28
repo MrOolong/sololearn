7ae4a16df151218e446c529b9e75cc4df0e92517
Dennis J Daubney Jr <dennis.daubney@gmail.com>
2017-12-28 17:44:40 -0500

    example of factorial method

---
+++ csharp/Factorial/Factorial/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Factorial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int test = Fact(5);
+            Console.WriteLine(test);
+        }
+
+        static int Fact(int num)
+        {
+            if (num == 1)
+            {
+                return 1;
+            }
+            return num * Fact(num - 1);
+        }
+    }
+}
